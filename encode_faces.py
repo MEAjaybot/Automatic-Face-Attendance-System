34e215177907465cb5cb7d42751581aac7072d96
MEAjaybot <ajaybk161@gmail.com>
2025-11-25 10:12:06 +0545

    code of emedding

--- encode_faces.py
+++ encode_faces.py
@@ -2,3 +2,10 @@ import cv2
 import torch
 import numpy as np
 from facenet_pytorch import MTCNN, InceptionResnetV1
+
+# Initialize face detector (MTCNN) and embedding model (FaceNet)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+mtcnn = MTCNN(keep_all=True, device=device)
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+
+print(torch.cuda.is_available())
